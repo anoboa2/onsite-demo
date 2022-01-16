94109880c40069c421ef392692daad40c444a836
anoboa2 <aanoboa14@yahoo.com>
2022-01-16 13:18:59 -0500

    initial commit

---
+++ src/modules/views/AppAppBar.js
@@ -0,0 +1,59 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import AppBar from '../components/AppBar';
+import Toolbar from '../components/Toolbar';
+
+const rightLink = {
+  fontSize: 16,
+  color: 'common.white',
+  ml: 3,
+};
+
+const onsiteLogo = 'https://onsite-images-1xo7eac.s3.amazonaws.com/Onsite_Logo_v03-08.png'
+
+function AppAppBar() {
+  return (
+    <div>
+      <AppBar position="fixed">
+        <Toolbar sx={{ justifyContent: 'space-between' }}>
+          <Box sx={{ flex: 1 }} />
+          <Link
+            variant="h6"
+            underline="none"
+            color="inherit"
+            href="/premium-themes/onepirate/"
+            sx={{ fontSize: 24 }}
+          >
+            <img 
+            src = {onsiteLogo} 
+            width = '100px'
+            />
+          </Link>
+          <Box sx={{ flex: 1, display: 'flex', justifyContent: 'flex-end' }}>
+            <Link
+              color="inherit"
+              variant="h6"
+              underline="none"
+              href="/premium-themes/onepirate/sign-in/"
+              sx={rightLink}
+            >
+              {'Log In'}
+            </Link>
+            <Link
+              variant="h6"
+              underline="none"
+              href="/premium-themes/onepirate/sign-up/"
+              sx={{ ...rightLink, color: 'secondary.main' }}
+            >
+              {'Sign Up'}
+            </Link>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+    </div>
+  );
+}
+
+export default AppAppBar;
---
+++ src/modules/views/ProductHero.js
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import Button from '../components/Button';
+import Typography from '../components/Typography';
+import ProductHeroLayout from './ProductHeroLayout';
+
+const backgroundImage =
+  'https://images.unsplash.com/photo-1415329343600-a53dd8daf392?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80';
+
+export default function ProductHero() {
+  return (
+    <ProductHeroLayout
+      sxBackground={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundColor: '#7fc7d9', // Average color of the background image.
+        backgroundPosition: 'center',
+      }}
+    >
+      {/* Increase the network loading priority of the background image. */}
+      <img
+        style={{ display: 'none' }}
+        src={backgroundImage}
+        alt="increase priority"
+      />
+      <Typography color="inherit" align="center" variant="h2" marked="center">
+      Travel booking, reinvented
+      </Typography>
+      <Typography
+        color="inherit"
+        align="center"
+        variant="h5"
+        sx={{ mb: 4, mt: { sx: 4, sm: 10 } }}
+      >
+        Get the most out of travel experience by leaving the planning to us
+      </Typography>
+      <Button
+        color="secondary"
+        variant="contained"
+        size="large"
+        component="a"
+        href="/premium-themes/onepirate/sign-up/"
+        sx={{ minWidth: 200 }}
+      >
+        Get started
+      </Button>
+      <Typography variant="body2" color="inherit" sx={{ mt: 2 }}>
+        From couch to Cabo at the click of a button
+      </Typography>
+    </ProductHeroLayout>
+  );
+}
