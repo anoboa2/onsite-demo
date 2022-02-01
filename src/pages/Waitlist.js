49aaec980038dce851c878205ae8c1fe904a4131
anoboa2 <aanoboa14@yahoo.com>
2022-02-01 18:20:03 -0500

    fixed poor performance with smaller screens

--- src/pages/Waitlist.js
+++ src/pages/Waitlist.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { styled } from '@mui/material/styles'
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 import Typography from '../modules/components/Typography';
 import WaitlistButton from '../modules/views/WaitlistButton';
 import withRoot from '../modules/withRoot';
@@ -14,11 +15,11 @@ const WaitlistRoot = styled(Container)(({ theme }) => ({
     position: 'relative',
     display: 'flex', 
     alignItems: 'center',
-    [theme.breakpoints.up('sm')]: {
-        height: '80vh',
-        minHeight: 500,
-        maxHeight: 1300,
-    },
+    // [theme.breakpoints.up('sm')]: {
+    //     height: '80vh',
+    //     minHeight: 500,
+    //     maxHeight: 1300,
+    // },
 }));
 
 const Background = styled(Box)(({ theme }) => ({
@@ -36,82 +37,41 @@ const Background = styled(Box)(({ theme }) => ({
 
 
 function WaitlistLanding() {
-    // const [ email, setEmail ] = React.useState("");
-    // const [ message, setMessage ] = React.useState("");
-
-    // const [ sent, setSent ] = React.useState(false);
-
-    // let handleSubmit = async (e) => {
-    //     e.preventDefault();
-    //     try {
-    //         let res = await fetch("https://httpbin.org/post", {
-    //             method: "POST",
-    //             body: JSON.stringify({
-    //                 email: email
-    //             }),
-    //         });
-        
-    //         let resJson = await res.json();
-    //         if (res.status === 200) {
-    //             setEmail("");
-    //             setMessage("Email successfully sent");
-    //         } else {
-    //             setMessage("Some error occurred");
-    //         }
-    //     } catch (err) {
-    //         console.log(err);
-    //     }
-    //     setSent(true);
-    // };
-
-
 
     return (
         <Background>
-            <WaitlistRoot>
-                <Container 
-                disableGutters
-                sx={({
-                    height: 400,
-                    maxWidth: 800,
-                    mb: '40vh',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'space-between',
-                })}
+            <Grid container space={2} justifyContent="center" alignItems="center">
+                <Typography
+                color="primary"
+                align="center"
+                variant="h1"
+                sx={{
+                    mx: 10,
+                    my: 4,
+                    fontWeight: 900,
+                    color: 'white',
+                }}
                 >
-                        <Typography
-                        color="primary"
-                        align="center"
-                        variant="h1"
-                        sx={{
-                            mx: 10,
-                            my: 4,
-                            fontWeight: 900,
-                            color: 'white',
-                        }}
-                        >
-                            Your personalized travel itinerary is coming sooner than you think!
-                        </Typography>
-                        <Typography 
-                        color="primary"
-                        align="center"
-                        variant="body1"
-                        sx={{
-                            display: 'flex',
-                            fontSize: 24,
-                            mx: 7,
-                            mb: 4,
-                            fontWeight: 400,
-                            color: 'white',
-                        }}
-                        >
-                            Be the first to access Onsite’s personalized travel planning. 
-                            We believe in helping you maximize every trip and harness the power of technology to curate the ultimate travel experiences.
-                        </Typography>
-                        <WaitlistButton />
-                </Container>
-            </WaitlistRoot>
+                    Your personalized travel itinerary is coming sooner than you think!
+                </Typography>
+                <Typography 
+                color="primary"
+                align="center"
+                variant="body1"
+                sx={{
+                    display: 'flex',
+                    fontSize: 24,
+                    mx: 7,
+                    mb: 4,
+                    fontWeight: 400,
+                    color: 'white',
+                }}
+                >
+                    Be the first to access Onsite’s personalized travel planning. 
+                    We believe in helping you maximize every trip and harness the power of technology to curate the ultimate travel experiences.
+                </Typography>
+                <WaitlistButton />
+            </Grid>
         </Background>
     )
 }
