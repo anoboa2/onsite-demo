19a50883a548626ff56e5042602ab05376d174fa
Oreoluwa Sola-Ojo <53820588+OreoluwaSolaOjo@users.noreply.github.com>
2022-03-28 09:35:56 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
-import Profile from './pages/Profile';
+import Waitlist from './pages/Waitlist';
 import Terms from './pages/Terms';
 import Privacy from './pages/Privacy';
+import Nyc from './pages/Nyc';
 import NewHomePage from "./pages/NewHomepageTravelPage/NewHomePage";
+import NewProfile from "./pages/NewHomepageTravelPage/NewTravelProfile/NewProfile";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<NewHomePage />} />
-      <Route path="/profile" element={<Profile />} />
+      {/* <Route path="/homeeee" element={<Home />} /> */}
+      {/* <Route path="/booking" element={<Booking />} /> */}
+      {/* <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} /> */}
+      <Route path="/waitlist" element={<Waitlist />} />
+      <Route path="/profile" element={<NewProfile />} />
       <Route path="/terms" element={<Terms />} />
       <Route path="/privacy" element={<Privacy />} />
+      <Route path="/nyc" element={<Nyc />} />
     </Routes>
   );
 }
