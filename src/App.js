dce60d6b64c41a6e48363327df0f64c4a2469ad5
gpravnkmr <gpravnkmr@gmail.com>
2022-04-10 20:14:38 -0400

    Add changes to how booking is done

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Terms from './pages/Terms';
 import Privacy from './pages/Privacy';
 import NewHomePage from "./pages/NewHomepageTravelPage/NewHomePage";
 import NewProfile from "./pages/NewHomepageTravelPage/NewTravelProfile/NewProfile";
+import NewBooking from "./pages/NewHomepageTravelPage/HeroImage/NewBooking";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<NewHomePage />} />
+      <Route path="/booking" element={<NewBooking />} />
       <Route path="/profile" element={<NewProfile />} />
       <Route path="/terms" element={<Terms />} />
       <Route path="/privacy" element={<Privacy />} />
