1e0b6d5957eea8526e0870c1fb7b63382c53195a
Angie Delgado <angiegdg@gmail.com>
2022-05-15 02:49:40 -0400

    Update NewAppBar.js
    Responsive AppBar

--- src/pages/NewHomepageTravelPage/NewAppBar.js
+++ src/pages/NewHomepageTravelPage/NewAppBar.js
@@ -7,6 +7,33 @@ import { useRownd } from '@rownd/react';
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { makeStyles } from "@mui/styles";
 
+import IconButton from '@mui/material/IconButton';
+import Typography from '../../modules/components/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+
+
+const pages = ['About Us', 'Blog', 'Profile', 'Sign Up'];
+
+
+const ResponsiveAppBar = () => {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+
+    
+    
 const rightLink = {
     fontSize: 16,
     color: 'secondary.main',
@@ -39,8 +66,9 @@ function NewAppBar() {
     const classes = useStyles()
     return (
         <div >
-            <AppBar className={classes.appbar} position="fixed">
-                <Toolbar sx={{ justifyContent: 'space-between' }}>
+            <AppBar className={classes.appbar} position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
                     <Box sx={{ flex: 0.1, justifyContent: 'flex-start' }} />
                     <Link
                         underline="none"
@@ -61,7 +89,45 @@ function NewAppBar() {
                             style={image}
                         />
                     </Link>
-                    <Box sx={{ flex: 1, display: 'flex', justifyContent: 'flex-end' }}>
+
+
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                       <IconButton
+                          size="large"
+                          aria-label="account of current user"
+                          aria-controls="menu-appbar"
+                          aria-haspopup="true"
+                          onClick={handleOpenNavMenu}
+                          color="inherit"
+                        >
+                          <MenuIcon />
+                        </IconButton>
+
+                        <Menu
+                          id="menu-appbar"
+                          anchorEl={anchorElNav}
+                          anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'left',
+                          }}
+                          keepMounted
+                          transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'left',
+                          }}
+                          open={Boolean(anchorElNav)}
+                          onClose={handleCloseNavMenu}
+                          sx={{
+                            display: { xs: 'block', md: 'none' },
+                          }}
+                        >
+                          {pages.map((page) => (
+                            <MenuItem key={page} onClick={handleCloseNavMenu}>
+                              <Typography textAlign="center">{page}</Typography>
+                            </MenuItem>
+                          ))}
+                        </Menu>
+
                         <Link
                             color="inherit"
                             variant="h6"
@@ -105,8 +171,8 @@ function NewAppBar() {
                         )}
                     </Box>
                 </Toolbar>
+            </Container>
             </AppBar>
-            <Toolbar />
         </div>
     );
 }
