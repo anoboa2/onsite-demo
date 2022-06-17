e7c0286e4b606d5968d642cfdab1dbdae4734a30
angiegdg <angiegdg@gmail.com>
2022-06-17 16:43:30 -0400

    speed website v4

--- src/App.js
+++ src/App.js
@@ -8,15 +8,19 @@ import NewBooking from "./pages/NewHomepageTravelPage/HeroImage/NewBooking";
 import AboutUs from "./pages/AboutUs";
 import LearnMore from "./pages/LearnMore";
 import NotFoundError from "./pages/NewHomepageTravelPage/NotFoundError";
+import axios from "axios";
 
 
-const axios = require('axios');
-const { axiosETAGCache } = require('axios-etag-cache');
-
-axiosETAGCache(axios)
-  .get('https://www.onsiteplanning.com/')
-  .then(console.log)
-  .catch(console.error);
+const currentVersion = 2
+let data = localStorage.getItem('data')
+let version = localStorage.getItem('version')
+if (!version || version < currentVersion) {
+  axios.get('/data').then((response) => {
+    data = response.data.data
+    localStorage.setItem('data', response.data.data)
+    localStorage.setItem('version', currentVersion)
+  })
+}
 
 function App() {
   return (
