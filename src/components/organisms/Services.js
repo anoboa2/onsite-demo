3d7f9acb167ce5dd5a06aabf8934d30717fcec81
Alex Noboa <aanoboa14@yahoo.com>
2022-08-16 14:47:46 -0400

    updated home content for mobile responsiveness

--- src/components/organisms/Services.js
+++ src/components/organisms/Services.js
@@ -5,7 +5,7 @@ import ServicesOffered from '../../content/ServicesOffered';
 const Services = () => {
 
   return (
-    <Grid container spacing={3} sx={{ px: "20%", py: 5, mb: 10, overflow: "hidden" }}>
+    <Grid container spacing={3} sx={{ px: {xs: 3, sm: "20%" }, py: 5, mb: 10, overflow: "hidden" }}>
       <Grid item xs={12} sx={{ display: "flex", flexDirection: "column", mb: 5 }}>
         <Typography variant="h2" align="center" sx={{ pb: 1 }}>We arrange the best</Typography>
         <Typography variant="subtitle1" align="center">Your specialist will plan your trip from start to finish</Typography>
