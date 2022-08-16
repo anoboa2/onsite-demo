051a2216ecd8ecd5d66b81b151ffe05faf46167a
Alex Noboa <aanoboa14@yahoo.com>
2022-08-16 16:26:17 -0400

    update for not found page mobile responsiveness

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -6,17 +6,16 @@ const NotFound = () => {
     return (
       <>
         <NavBar />
-          <Grid container spacing={12} sx={{ mt: '5vh', height: '100vh', px: 20}}>
-            <Grid item xs={6} sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+          <Grid container spacing={{xs: 1, sm: 3}} sx={{ pt: '25%', height: '100vh', px: { xs: 1, sm: 20 } }}>
+            <Grid item xs={12} sm={6} sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
               <Typography variant="h4">
                 OOPS!!! <Box component="span" sx={{ color: '#0FAACD' }}>Page Not Found</Box>
               </Typography>
               <Typography variant="h5" sx={{mt: 4}} style={{ textAlign: "justify" }}>
                   Sorry, the page you are looking for could not be found.
               </Typography>
-              <Box sx={{ py: 18 }}/>
             </Grid>
-            <Grid item xs={6}>
+            <Grid item xs={12} sm={6}>
               <Box
                   component="img"
                   src="/img/Error404.png"
