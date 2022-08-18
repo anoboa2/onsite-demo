42c4ed44e7137c972abcd98e76eeef9dc9be3028
Alex Noboa <aanoboa14@yahoo.com>
2022-08-18 13:40:14 -0400

    convered to a map function

--- src/components/organisms/FAQSection.js
+++ src/components/organisms/FAQSection.js
@@ -2,45 +2,38 @@ import { Box, Grid, Typography } from '@mui/material';
 import AccordionItem from '../molecules/AccordionItem';
 import { AboutSection , SpecialistSection, TravelerSection } from '../../content/FAQs';
 
+const Sections = [
+  {
+    title: 'About Onsite',
+    content: AboutSection,
+  },
+  {
+    title: 'Specialist',
+    content: SpecialistSection,
+  },
+  {
+    title: 'Travelers',
+    content: TravelerSection,
+  },
+]
+
 const FAQSection = () => {
   return (
-    <Grid container spacing={3} sx={{ px: 10 }}>
-      <Grid item xs={12} md={6} sx={{ mt: 6, display: 'flex', justifyContent: "space-around", borderRadius: 50 }}>
-      <Box sx={{ display: 'flex', flexDirection: "column" }}>
-        <Typography variant="h4" component="h1" align="center">
-          About Onsite
-        </Typography>
-        <Box sx={{ px: "15%" }}>
-          {AboutSection.map((faq) => (
-            <AccordionItem key={faq.order} content={faq} />
-          ))}
-        </Box>
-      </Box>
-      </Grid>
-      <Grid item xs={12} md={6} sx={{ mt: 6, display: 'flex', justifyContent: "space-around", borderRadius: 50 }}>
-      <Box sx={{ display: 'flex', flexDirection: "column" }}>
-        <Typography variant="h4" component="h1" align="center">
-          Specialist
-        </Typography>
-        <Box sx={{ px: "15%" }}>
-          {SpecialistSection.map((faq) => (
-            <AccordionItem key={faq.order} content={faq} />
-          ))}
-        </Box>
-      </Box>
-      </Grid>
-      <Grid item xs={12} md={6} sx={{ mt: 6, display: 'flex', justifyContent: "space-around", borderRadius: 50 }}>
-      <Box sx={{ display: 'flex', flexDirection: "column" }}>
-        <Typography variant="h4" component="h1" align="center">
-          Travelers
-        </Typography>
-        <Box sx={{ px: "15%" }}>
-          {TravelerSection.map((faq) => (
-            <AccordionItem key={faq.order} content={faq} />
-          ))}
-        </Box>
-      </Box>
-      </Grid>
+    <Grid container spacing={3} sx={{ py: 10, px: {xs: 1, sm: 3, md: 8} }}>
+      {Sections.map((section) => (
+          <Grid item xs={12} sx={{ mt: 6, display: 'flex', justifyContent: "center", borderRadius: 50 }}>
+          <Box sx={{ display: 'flex', flexDirection: "column" }}>
+            <Typography variant="h4" component="h1" align="center">
+              {section.title}
+            </Typography>
+            <Box sx={{ px: "10%" }}>
+              {section.content.map((faq) => (
+                <AccordionItem key={faq.order} content={faq} />
+              ))}
+            </Box>
+          </Box>
+          </Grid>
+      ))}
     </Grid>
   )
 }
