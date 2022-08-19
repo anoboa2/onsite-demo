d21b0dc72338e02d98f06a231e529a7c279e5381
Alex Noboa <aanoboa14@yahoo.com>
2022-08-19 10:08:01 -0600

    added press page

---
+++ src/content/PressLinks.js
@@ -0,0 +1,18 @@
+const PressLinks = [
+  {
+    order: 1,
+    publication: 'Shoutout Miami',
+    date: 'April 6, 2022',
+    title: 'Meet Izanna Levintova | Founder & CEO',
+    link: 'https://shoutoutmiami.com/meet-izanna-levintova-founder-ceo/',
+  },
+  {
+    order: 2,
+    publication: 'Viral Proteins',
+    date: 'June 01, 2022',
+    title: 'Travel experts share 10 underrated summer vacation spots',
+    link: 'https://www.vitalproteins.com/blogs/lifestyle/summer-bucket-list#'
+  }
+]
+
+export default PressLinks;
