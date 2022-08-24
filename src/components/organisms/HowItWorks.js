bc672ffa078e25573500df96241875cf12e39103
Alex Noboa <55636642+anoboa2@users.noreply.github.com>
2022-08-23 23:10:09 -0400

    fixing remaining changes

--- src/components/organisms/HowItWorks.js
+++ src/components/organisms/HowItWorks.js
@@ -1,34 +1,6 @@
 import { Box, Card, Grid, Typography } from '@mui/material';
-
+import PlanningSteps from '../../content/PlanningSteps';
 import { makeStyles } from "@mui/styles";
-
-
-const content = [
-  {
-    order: 1,
-    title: 'Submit',
-    description: '• Fill out our booking form to get matched with a specialist from your desired destination',
-    description2: '• Your specialist will reach out to you to confirm all details necessary to begin creating your perfect trip itinerary',
-    icon: '/icon/finger-touching-tablet-screen.png',
-    alt: 'fully packed station wagon car'
-  },
-  {
-    order: 2,
-    title: 'Match',
-    description: '• Allow 48 hours for a draft itinerary to be sent to you',
-    description2: '• Chat with your personal specialist to provide any feedback, questions or changes',
-    description3: '• Your specialist will quickly provide updates until you are 100% satisfied',
-    icon: '/icon/support.png',
-    alt: 'two airplane tickets'
-  },
-  {
-    order: 3,
-    title: 'Collaborate',
-    description: '• Approve the itinerary and we will provide you with a Stripe invoice for the full amount of the trip plus our 7% service fee. Once we receive payment, your specialist will complete all the bookings.',
-    icon: '/icon/money.png',
-    alt: 'plane in flight'
-  }
-];
 
 const cardStyle = {
   display: 'flex',
@@ -68,14 +40,12 @@ const HowItWorks = () => {
     <Grid item xs={12} className={classes.cards}>
       <Typography variant="h2" align='center' sx={{ px: { xs: 2, sm: "20%" }, py: 5, mb: -3, ml: -5 , mr: -5, overflow: "hidden" }}>How Onsite Works<Box component="span" sx={{ color: "primary.main", borderBottom: "3px solid #0FAACD" }}> For You</Box></Typography>
     </Grid>
-      {content.map(item => (
+      {PlanningSteps.map(item => (
         <Grid item xs={28} sm={6} md={4} key={item.order}>
           <Card sx={cardStyle}>
             <Box sx={{ display: 'flex', justItems: "center", justifyContent: 'center', alignItems: 'center', alignContent: "center", flexDirection: 'column', py: 2 }}>
               <Box component="img" src={item.icon} alt={item.alt} sx={{ height: 90, width: 90, my: 2, mx: 'auto'}}/>
-              <Typography variant='body1' align="left">{item.description}</Typography><br />
-              <Typography variant='body1' align="left">{item.description2}</Typography><br />
-              <Typography variant='body1' align="left">{item.description3}</Typography>
+              <Typography variant='body1' align="left">{item.description}</Typography>
             </Box>
           </Card>
         </Grid>
