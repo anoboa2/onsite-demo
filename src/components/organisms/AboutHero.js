af02053fc7a8a7c013e27adb502cd5b7e233e861
angiegdg <angiegdg@gmail.com>
2022-08-24 21:29:26 -0400

    about us new design v1

--- src/components/organisms/AboutHero.js
+++ src/components/organisms/AboutHero.js
@@ -8,7 +8,7 @@ const AboutHero = () => {
          <Typography variant="h1" fontWeight="300" fontStyle= "italic" align="center" sx={{ py: 1}}>Stress Less, Dream More.</Typography>
          <Container sx={{ mt: { xs: 8, sm: 10 } , mb: { xs: 8, sm: 10 }  , justifyContent:'center', ml: { xs: -7, sm: -16 } }}>
         <img 
-        src='/img/content/franck-charles.JPG' 
+        src='/img/content/franck-charles.jpg' 
         style={{
           height:"500px" ,
           width:"145%",
