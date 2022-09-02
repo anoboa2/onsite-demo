6af115351e7d9a5c9a165afc7202dfb0470e9b4c
angiegdg <angiegdg@gmail.com>
2022-09-02 00:53:29 -0400

    updated preview itinerary v5

--- src/components/templates/Specialist.js
+++ src/components/templates/Specialist.js
@@ -4,8 +4,6 @@ import { Box, Grid, Typography } from '@mui/material';
 import ProfileCard from '../molecules/ProfileCard';
 import ItineraryCard from '../molecules/ItineraryCard';
 import SpecialistHero from '../organisms/SpecialistHero';
-import ReactReadMoreReadLess from "react-read-more-read-less";
-import { fontWeight } from '@mui/system';
 
 const Specialist = () => {
   const { id } = useParams();
@@ -55,11 +53,11 @@ const Specialist = () => {
                     {specialist.favorite_destination}
                   </Typography>
                 </Grid>
-                <Grid item xs={12} sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' , mx: {md: 10}, mt: {sm: -63} , paddingLeft:{xs: 10 , sm:0} , paddingRight:{xs: 13, md:0} }}>
+                <Grid item xs={12} sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' , mx: {md: 10}, paddingLeft:{xs: 10 , sm:0} , paddingRight:{xs: 13, md:0} }}>
                   <Typography variant="h3" color="primary.main" sx={{mr:{xs:-8}}}  >
                     Travel tips from {specialist.first_name}
                   </Typography>
-                  <Box sx={{ width: {sm: '85%' , xs: "170%"}, mb: {xs: 5, sm: -5}, justifyContent: 'center', alignContent: 'center', ml:{xs:-2, sm:7} , visibility:{xs:'hidden', sm: 'visible'}  }} >
+                  <Box sx={{ width: {sm: '85%' , xs: "170%"},  mb: {xs: 5, sm: -5}, justifyContent: 'center', alignContent: 'center', ml:{xs:-2, sm:7}  }} >
                     <Box component="ul" >
                       {specialist.tips && specialist.tips.map(tip => (
                         <Box  component="li" key={tip} >
@@ -68,29 +66,10 @@ const Specialist = () => {
                       ))}
                     </Box>
                   </Box>
-                  <Box sx={{ width: {xs: "170%"},  mb: {xs: 2}, mt: {xs: -205}, justifyContent: 'center', alignContent: 'center', ml:{xs:-2, sm:7} , visibility:{sm:'hidden', xs: 'visible'}  }} >
-                    <Box component="ul" >
-                      {specialist.tips && specialist.tips.map(tip => (
-                        <Box component="li" key={tip} typography="body1" fontWeight={300} paddingBottom={1} lineHeight={1.2} >
-                           <ReactReadMoreReadLess
-                            charLimit={50}
-                            readMoreText={"Read more +"}
-                            readLessText={"Read less -"}
-                            readMoreClassName="read-more-less--more"
-                            readLessClassName="read-more-less--less"
-                          >
-                            
-                            {tip}
-                          </ReactReadMoreReadLess>
-                          {/* <Typography variant="body1" fontWeight={300} paddingBottom={1} >{tip}</Typography> */}
-                        </Box>
-                      ))}
-                    </Box>
-                  </Box>
                 </Grid>
               </Grid>
             </Box>
-            <Grid container spacing={4} sx={{ width: {xs: "185%" , sm: "100%"}, my: 10, px: { xs: 3, sm: "10%" } , alignContent: 'center', py: 5, mb: 10, mt: { xs: -8, sm: 72 }, mb:{xs: 7 , sm: 1} , justifyContent: 'center' }}>
+            <Grid container spacing={4} sx={{ width: {xs: "185%" , sm: "100%"}, my: 10, px: { xs: 3, sm: "10%" } , alignContent: 'center', py: 5, mb: 10, mt: { xs: -5, sm: -19 }, mb:{xs: 7 , sm: 1} , justifyContent: 'center' }}>
               <Grid item xs={12}>
                 <Typography variant="h2" fontWeight="300" align='center' sx={{ px: { xs: 2, sm: "20%" }, py: 5, mb:{xs: -2 , sm: -1}, ml: -5 , mr: -5, overflow: "hidden" }}>
                   Recent <Box component="span" sx={{ color:"primary.main"}} ><b>Itineraries</b></Box>
