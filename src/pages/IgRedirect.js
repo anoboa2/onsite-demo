291f0c854a5eb03bcf144fce2f459ffd75eb6c64
Alex Noboa <aanoboa14@yahoo.com>
2022-09-12 11:30:45 -0400

    testing ig login

--- src/pages/IgRedirect.js
+++ src/pages/IgRedirect.js
@@ -2,11 +2,16 @@ import NavBar from '../components/organisms/NavBar';
 import Footer from '../components/organisms/Footer';
 
 const IgRedirect = () => {
+  // get query parameters
+  const params = new URLSearchParams(window.location.search);
+  const code = params.get('code');
+
   return (
     <>
       <NavBar />
       <div>
         <h2>Redirected from Instagram</h2>
+        {code && <p>Code: {code}</p>}
       </div>
       <Footer />
     </>
