585e9daed8585aa5642c4485587dd5afcdada0d2
Alex Noboa <aanoboa14@yahoo.com>
2022-09-13 11:57:48 -0400

    adding token storage

--- src/pages/IgRedirect.js
+++ src/pages/IgRedirect.js
@@ -1,6 +1,7 @@
 import { Box, Button, Typography } from '@mui/material';
 import NavBar from '../components/organisms/NavBar';
 import Footer from '../components/organisms/Footer';
+import { useEffect } from 'react';
 
 const appId = '1071094180207728';
 const redirectUri = 'https://www.onsiteplanning.com/igredirect/';
@@ -11,6 +12,33 @@ const IgRedirect = () => {
   // get query parameters
   const params = new URLSearchParams(window.location.search);
   const code = params.get('code');
+
+  const storeToken= (payload) => {
+    fetch('https://hmou3ha9b1.execute-api.us-east-1.amazonaws.com/v1/profile/storeinstagramtoken', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload)
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Success:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+
+
+  useEffect(() => {
+    storeToken({
+      user: 'noboa',
+      platform: 'instagram',
+      code: code
+    })
+  }, [code]);
+  
 
   return (
     <>
