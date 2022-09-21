b61fc30d166e83c76b776a18149a887c0ef86455
angiegdg <angiegdg@gmail.com>
2022-09-21 14:28:25 -0400

    updates generals

--- src/pages/ComingSoon.js
+++ src/pages/ComingSoon.js
@@ -8,7 +8,7 @@ const ComingSoon = () => {
         <NavBar />
           <Grid container spacing={3} sx={{ px: { xs: 3, sm: "20%" }, py: 5, mb: 10, mt: { xs: 5, sm: 3 }, overflow: "hidden" }}>
             <Grid item xs={12} sm={6} sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
-              <Typography variant="h4">
+              <Typography variant="h3">
                 NEW PAGE!!! <Box component="span" sx={{ color: '#0FAACD' }}>Coming Soon</Box>
               </Typography>
               <Typography fontWeight={300} variant="h5" sx={{mt: 4}} style={{ textAlign: "justify" }}>
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -8,7 +8,7 @@ const NotFound = () => {
         <NavBar />
           <Grid container spacing={3} sx={{ px: { xs: 3, sm: "20%" }, py: 5, mb: 10, mt: { xs: 10, sm: 10 }, overflow: "hidden" }}>
             <Grid item xs={12} sm={6} sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
-              <Typography variant="h4">
+              <Typography variant="h3">
                 OOPS!!! <Box component="span" sx={{ color: '#0FAACD' }}>Page Not Found</Box>
               </Typography>
               <Typography fontWeight={300} variant="h5" sx={{mt: 4}} style={{ textAlign: "justify" }}>
