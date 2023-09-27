e9c86e4c65a6bf95198f6c59d023716cd77463e6
Alex Noboa <aanoboa14@yahoo.com>
2023-09-27 00:06:28 -0400

    removed page overflow

--- src/components/organisms/HowItWorks.js
+++ src/components/organisms/HowItWorks.js
@@ -5,10 +5,10 @@ import PlanningSteps from '../../content/PlanningSteps';
 const HowItWorks = () => {
   return (
     <Box sx={{ display: 'block', maxWidth: '1440px', margin: 'auto'}} >
-      <Typography variant="h2" align="center" sx={{ px: { xs: 2, sm: "20%" }, py: 5, mb: -1, ml: -5 , mr: -5, overflow: "hidden" , color:"primary.main" }}>
-        How Onsite works <Box component="span" sx={{ color:"#0FAACD"}}>for you</Box>
+      <Typography variant="h2" align="center" sx={{ px: { xs: 2, sm: "20%" }, py: 5, mb: -1, color:"primary.main" }}>
+        How Onsite works for you
       </Typography>
-      <Grid container spacing={3} sx={{ justifyContent: 'space-evenly', mb: 13 }}>
+      <Grid container spacing={3} sx={{ justifyContent: 'space-evenly', mb: 13, maxWidth: '100%', overflow: 'hidden' }}>
         {PlanningSteps.map(item => (
           <Grid xs={12} sm={6} md={3} key={item.order} sx={{ display: 'flex', justifyContent: 'center' }}>
             <Card sx={{ px: 3, height: '350px', width: '280px', borderRadius: '30px', transition: 'all 0.15s ease-in-out', boxShadow: "12px 12px 45px rgba(0,0,0,.1)", '&:hover': {transform: 'scale(1.05)'}}}>
